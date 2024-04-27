391b7cedaf3b8fb35c287e97f02fd58187face83
pptx704 <rafeedm.bhuiyan@gmail.com>
2024-04-27 15:11:01 +0300

    Merge dependencies

--- backend/const.py
+++ backend/const.py
@@ -1,3 +1,10 @@
-import sqlite3
+import os
+from dotenv import load_dotenv
 
-SQLITE_DB = 'sqlite:///db.sqlite3'
+load_dotenv()
+
+SQLITE_DB = os.environ.get("SQLITE_DB", 'sqlite:///db.sqlite3')
+
+SECRET_KEY = os.environ.get("JWT_SECRET")
+ALGORITHM = os.environ.get("JWT_ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("JWT_EXPIRE_MINUTES", 240))
