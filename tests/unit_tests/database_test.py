449e74e042be5933eb2caf395fc976f313107b93
PATH242 <mennashaheen2357@gmail.com>
2024-04-27 19:10:21 +0300

    lint database tests

--- tests/unit_tests/database_test.py
+++ tests/unit_tests/database_test.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import MagicMock
-import sqlite3
 import backend.database as db
+
 
 class TestDatabaseBookFunctions(unittest.TestCase):
 
@@ -14,15 +14,16 @@ class TestDatabaseBookFunctions(unittest.TestCase):
             (1, 'Sample Book', 'Author A', 'Genre A'),
             (2, 'Another Book', 'Author B', 'Genre B')
         ]
-        self.cursor.fetchone.return_value = (1, 'Sample Book', 'Author A', 'Genre A')
+        self.cursor.fetchone.return_value = (
+            1, 'Sample Book', 'Author A', 'Genre A')
 
     def test_create_book(self):
         db.create_book('Test Book', 'Test Author', 'Test Genre', self.conn)
         self.conn.execute.assert_called_with(
-        """
+            """
         INSERT INTO books (title, author, genre)
         VALUES (?, ?, ?)\n    """,
-        ('Test Book', 'Test Author', 'Test Genre')
+            ('Test Book', 'Test Author', 'Test Genre')
         )
         self.conn.commit.assert_called_once()
 
@@ -31,8 +32,9 @@ class TestDatabaseBookFunctions(unittest.TestCase):
         self.conn.execute.assert_called_with("""
         SELECT * FROM books LIMIT ? OFFSET ?
     """, (2, 0))
-        self.assertEqual(books, [(1, 'Sample Book', 'Author A', 'Genre A'), (2, 'Another Book', 'Author B', 'Genre B')])
-    
+        self.assertEqual(books, [(1, 'Sample Book', 'Author A', 'Genre A'),
+                                 (2, 'Another Book', 'Author B', 'Genre B')])
+
     def test_get_genres(self):
         self.cursor.fetchall.return_value = [('Fiction',), ('Non-Fiction',)]
         genres = db.get_genres(self.conn)
@@ -48,7 +50,7 @@ class TestDatabaseBookFunctions(unittest.TestCase):
         SELECT * FROM books WHERE id = ?
     """, (book_id,))
         self.assertEqual(book, (1, 'Sample Book', 'Author A', 'Genre A'))
-        
+
     def test_get_book_count(self):
         self.cursor.fetchone.return_value = (5,)
         count = db.get_book_count(self.conn)
@@ -56,13 +58,16 @@ class TestDatabaseBookFunctions(unittest.TestCase):
         SELECT COUNT(*) FROM books
     """)
         self.assertEqual(count, 5)
+
     def test_search_book_by_title(self):
         search_title = 'Sample'
         searched_books = db.search_book_by_title(search_title, self.conn)
         self.conn.execute.assert_called_with("""
         SELECT * FROM books WHERE title LIKE ?
     """, ("%Sample%",))
-        self.assertEqual(searched_books, [(1, 'Sample Book', 'Author A', 'Genre A'), (2, 'Another Book', 'Author B', 'Genre B')])
+        self.assertEqual(searched_books, [
+            (1, 'Sample Book', 'Author A', 'Genre A'),
+            (2, 'Another Book', 'Author B', 'Genre B')])
 
     def test_get_books_by_genre(self):
         genre = 'Fiction'
@@ -70,10 +75,13 @@ class TestDatabaseBookFunctions(unittest.TestCase):
         self.conn.execute.assert_called_with("""
         SELECT * FROM books WHERE genre = ?
     """, (genre,))
-        self.assertEqual(books_by_genre, [(1, 'Sample Book', 'Author A', 'Genre A'), (2, 'Another Book', 'Author B', 'Genre B')])
+        self.assertEqual(books_by_genre, [
+                         (1, 'Sample Book', 'Author A', 'Genre A'),
+                         (2, 'Another Book', 'Author B', 'Genre B')])
 
     def test_update_book(self):
-        db.update_book(1, 'Updated Book', 'Updated Author', 'Updated Genre', self.conn)
+        db.update_book(1, 'Updated Book', 'Updated Author',
+                       'Updated Genre', self.conn)
         self.conn.execute.assert_called_with("""
         UPDATE books
         SET title = ?, author = ?, genre = ?, updated_at = CURRENT_TIMESTAMP
@@ -95,6 +103,7 @@ class TestDatabaseBookFunctions(unittest.TestCase):
         self.conn.execute.assert_has_calls(calls, any_order=True)
         self.conn.commit.assert_called_once()
 
+
 class TestDatabaseUserFunctions(unittest.TestCase):
     def setUp(self):
         # Set up a mocked sqlite3 connection and cursor
@@ -108,12 +117,12 @@ class TestDatabaseUserFunctions(unittest.TestCase):
         self.cursor.fetchone.return_value = (1, 'user1', 'hash1')
 
     def test_create_user(self):
-            db.create_user('newuser', 'newhash', self.conn)
-            self.conn.execute.assert_called_with("""
+        db.create_user('newuser', 'newhash', self.conn)
+        self.conn.execute.assert_called_with("""
         INSERT INTO users (username, password_hash)
         VALUES (?, ?)
     """, ('newuser', 'newhash'))
-            self.conn.commit.assert_called_once()
+        self.conn.commit.assert_called_once()
 
     def test_get_users(self):
         users = db.get_users(self.conn)
@@ -129,19 +138,19 @@ class TestDatabaseUserFunctions(unittest.TestCase):
         SELECT * FROM users WHERE id = ?
     """, (user_id,))
         self.assertEqual(user, (1, 'user1', 'hash1'))
-    
+
     def test_get_user_by_username(self):
         username = 'user1'
         user = db.get_user_by_username(username, self.conn)
-        
+
         self.conn.execute.assert_called_with("""
         SELECT * FROM users WHERE username = ?
     """, (username,))
-        
+
         self.assertEqual(user, (1, 'user1', 'hash1'))
 
     def test_update_user(self):
-        db.update_user(1, 'updateduser', 'updatedhash',  self.conn)
+        db.update_user(1, 'updateduser', 'updatedhash', self.conn)
         self.conn.execute.assert_called_with("""
         UPDATE users
         SET username = ?, password_hash = ?, updated_at = CURRENT_TIMESTAMP
@@ -161,6 +170,7 @@ class TestDatabaseUserFunctions(unittest.TestCase):
         ]
         self.conn.execute.assert_has_calls(calls, any_order=True)
         self.conn.commit.assert_called_once()
+
 
 class TestDatabaseReadingListFunctions(unittest.TestCase):
 
@@ -188,7 +198,9 @@ class TestDatabaseReadingListFunctions(unittest.TestCase):
         self.conn.execute.assert_called_with("""
         SELECT * FROM reading_list WHERE user = ?
     """, (1,))
-        self.assertEqual(reading_lists, [(1, 1, 1, 'not_started'), (2, 1, 2, 'in_progress')])
+        self.assertEqual(
+            reading_lists, [(1, 1, 1, 'not_started'),
+                            (2, 1, 2, 'in_progress')])
 
     def test_get_completed_books(self):
         # Change the return value for this specific test
@@ -197,24 +209,31 @@ class TestDatabaseReadingListFunctions(unittest.TestCase):
             (2, 1, 2, 'complete')
         ]
         completed_books = db.get_completed_books(1, self.conn)
-        self.conn.execute.assert_called_with("""
-        SELECT * FROM reading_list WHERE user = ? AND reading_status = 'complete'
-    """,  (1,))
-        self.assertEqual(completed_books, [(1, 1, 1, 'complete'), (2, 1, 2, 'complete')])
+        self.conn.execute.assert_called_with(
+            """
+        SELECT * FROM reading_list WHERE user = ?
+        AND reading_status = 'complete'
+    """,
+            (1,))
+        self.assertEqual(completed_books, [
+                         (1, 1, 1, 'complete'), (2, 1, 2, 'complete')])
 
     def test_get_readers(self):
         readers = db.get_readers(1, self.conn)
         self.conn.execute.assert_called_with("""
         SELECT * FROM reading_list WHERE book = ?
     """, (1,))
-        self.assertEqual(readers, [(1, 1, 1, 'not_started'), (2, 1, 2, 'in_progress')])
+        self.assertEqual(
+            readers, [(1, 1, 1, 'not_started'), (2, 1, 2, 'in_progress')])
 
     def test_get_book_read_count(self):
         self.cursor.fetchone.return_value = (5,)
         n = db.get_book_read_count(1, self.conn)
-        self.conn.execute.assert_called_with("""
-        SELECT COUNT(*) FROM reading_list WHERE book = ? AND reading_status = 'complete'
-    """,  (1,))
+        self.conn.execute.assert_called_with(
+            """
+        SELECT COUNT(*) FROM reading_list WHERE book = ?
+        AND reading_status = 'complete'
+    """, (1,))
         assert n == 5
 
     def test_remove_from_reading_list(self):
@@ -232,6 +251,7 @@ class TestDatabaseReadingListFunctions(unittest.TestCase):
         WHERE book = ? AND user = ?
     """, ('completed', 1, 1))
         self.conn.commit.assert_called_once()
+
 
 if __name__ == "__main__":
     unittest.main()
